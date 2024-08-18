bc09394a600290e42e9107dd5540fecf7b1c11cb
Nelson Isidro Jr. <isidronelson454@gmail.com>
2024-08-19 03:28:54 +0800

    Initial Commit: Added the function of moving file from folder 1 to folder 2 using FileSystemWatcher

---
+++ Exam2WindowsService/WindowsServiceFileMonitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exam2WindowsService
+{
+    public partial class WindowsServiceFileMonitor : ServiceBase
+    {
+        private FileSystemWatcher folder1Watcher;
+        private readonly string sourceFolderPath = @"C:\Folder1";
+        private readonly string destinationFolderPath = @"C:\Folder2";
+
+        public WindowsServiceFileMonitor()
+        {
+            InitializeComponent();
+            ServiceName = "WindowsServiceFileMonitor";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            folder1Watcher = new FileSystemWatcher(sourceFolderPath)
+            {
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                Filter = "*.*",
+                EnableRaisingEvents = true
+            };
+
+            folder1Watcher.Created += OnFileCreated;
+
+        }
+
+        private void OnFileCreated(object sender, FileSystemEventArgs e)
+        {
+            // Move the file to Folder2 when created
+            string destinationFilePath = Path.Combine(destinationFolderPath, Path.GetFileName(e.FullPath));
+            File.Move(e.FullPath, destinationFilePath);
+        }
+
+        protected override void OnStop()
+        {
+            folder1Watcher.Dispose();
+        }
+    }
+}
