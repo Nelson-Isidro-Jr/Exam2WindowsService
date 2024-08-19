e6abd6a6fd5af99f1462da28396796c9c7a865b4
Nelson Isidro Jr. <isidronelson454@gmail.com>
2024-08-19 12:03:34 +0800

    Added 4 second timer to add and edit file before transfer to folder2, added rename files that has the same name incrementing it by 1. Issue Resolved: it accepts files with the same file type

--- Exam2WindowsService/WindowsServiceFileMonitor.cs
+++ Exam2WindowsService/WindowsServiceFileMonitor.cs
@@ -25,27 +25,52 @@ namespace Exam2WindowsService
 
         private void SetupFileSystemWatcher()
         {
+
             folder1Watcher = new FileSystemWatcher(sourceFolderPath)
             {
+                EnableRaisingEvents = true,
                 NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
                 Filter = "*.*"
             };
             folder1Watcher.Created += OnFileCreated;
 
-
         }
 
         protected override void OnStart(string[] args)
         {
-            folder1Watcher.EnableRaisingEvents = true;
             SetupFileSystemWatcher();
         }
 
         private void OnFileCreated(object sender, FileSystemEventArgs e)
         {
-            // Move the file to Folder2 when created
-            string destinationFilePath = Path.Combine(destinationFolderPath, Path.GetFileName(e.FullPath));
-            File.Move(e.FullPath, destinationFilePath);
+            try
+            {
+                
+                System.Threading.Thread.Sleep(4000);
+
+                // Construct the destination file path
+                string destinationFilePath = Path.Combine(destinationFolderPath, Path.GetFileName(e.FullPath));
+
+                // Check if the file already exists and rename if necessary
+                int count = 1;
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(e.FullPath);
+                string extension = Path.GetExtension(e.FullPath);
+
+                while (File.Exists(destinationFilePath))
+                {
+                    string tempFileName = $"{fileNameWithoutExtension}_{count}{extension}";
+                    destinationFilePath = Path.Combine(destinationFolderPath, tempFileName);
+                    count++;
+                }
+
+               
+                File.Move(e.FullPath, destinationFilePath);
+            }
+            catch (Exception ex)
+            {
+               
+                EventLog.WriteEntry("WindowsServiceFileMonitor", $"Error moving file: {ex.Message}", EventLogEntryType.Error);
+            }
         }
 
         protected override void OnStop()
